37bc718dad3f694b2076b10160335f00295677e8
Mujaffar Kayyum Shaikh <35863495+mujaffar98@users.noreply.github.com>
2024-10-29 10:19:44 +0530

    Add files via upload

---
+++ weather_script.js
@@ -0,0 +1,45 @@
+let apikey = "aff21841344490e5c6282ad8239b87ec";
+let apiURL = "https://api.openweathermap.org/data/2.5/weather?";
+let city;
+let userCity = document.getElementById("userText");
+let temperature = document.getElementById("temp");
+let ct = document.getElementById("city");
+let humidity = document.getElementById("humditiy-info");
+let windInfo = document.getElementById("wind-info");
+let weatherImage = document.querySelector(".weather_img")
+
+async function getWeatherData(){
+    if (userCity.value==""){
+        alert("Enter City Name")
+    }
+    else{
+        city = userCity.value;
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`);
+        let data = await response.json();
+        ct.innerHTML = data.name;
+        temperature.innerHTML = Math.round(data.main.temp - 273.15)+"°C";
+        humidity.innerHTML = data.main.humidity + "%";
+        windInfo.innerHTML = data.wind.speed + "km/h";
+        userCity.value="";
+
+        if(data.weather[0].main=="Clear"){
+            weatherImage.src = "assets/clear.png";
+        }
+        else if(data.weather[0].main=="Snow"){
+            weatherImage.src = "assets/snow.png";
+        }
+        else if(data.weather[0].main=="Cloud"){
+            weatherImage.src = "assets/cloud.png";
+        }
+        else if(data.weather[0].main=="Drizzle"){
+            weatherImage.src = "assets/drizzle.jpg";
+        }
+        else if(data.weather[0].main=="Mist"){
+            weatherImage.src = "assets/mist.png";
+        }
+        else if(data.weather[0].main=="Rain"){
+            weatherImage.src = "assets/rain.png";
+        }
+        console.log(data)
+    }
+}
